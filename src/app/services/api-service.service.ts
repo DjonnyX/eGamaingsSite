a5127a1e30f51bf319cf7a2ceec9ede5284ebb9c
djonnyx <djonnyx@gmail.com>
2020-04-07 12:08:48 +0300

    feat: created api-service

---
+++ src/app/services/api-service.service.ts
@@ -0,0 +1,70 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { ID } from '@datorama/akita';
+import {IGameModel} from '../models/IGameModel';
+import { createRequestParams, IRequestParams } from '../utils/srv-request.util';
+
+const HTTP_OPTIONS = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+};
+
+
+const API_URL = '/api/';
+
+enum ApiRouteNames {
+  GAMES = 'games',
+}
+
+export class ApiRoutes {
+  private static _games = `${API_URL}${ApiRouteNames.GAMES}`;
+
+  static get GAMES() { return ApiRoutes._games; }
+}
+
+interface IGameFilterParams {
+  id?: number;
+  name?: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+
+  constructor(private _http: HttpClient) { }
+
+  /**
+   * Выполняет GET-запрос к json-server'у с параметрами фильтрации, сортировки, etc
+   */
+  protected get<T, F>(url: string, options: IRequestParams<F>) {
+    const opt = createRequestParams(options);
+    return this._http.get<T>(url, {
+      params: opt,
+      observe: 'response'
+    }).pipe(
+      map(res => ({
+          total: Number(res.headers.get('X-Total-Count')),
+          items: res.body
+        })
+      )
+    )
+  }
+
+  /**
+   * Получение списка игр с заданными параметрами фильтрации
+   */
+  getGames(filter?: IRequestParams<IGameFilterParams>) {
+    return this.get<Array<IGameModel>, IGameFilterParams>(ApiRoutes.GAMES, filter);
+  }
+
+  /**
+   * Обновление выбранной игры
+   * Для выставления флага <code>isFavorite</code>
+   */
+  updateGames(game: IGameModel) {
+    return this._http.put<IGameModel>(`${ApiRoutes.GAMES}/${game.id}`, game);
+  }
+}
---
+++ src/app/utils/srv-request.util.ts
@@ -0,0 +1,71 @@
+interface IQueryParams { [param: string]: string | string[] }
+
+/**
+ * Интерфейс параметров запроса
+ */
+export interface IRequestParams<F> {
+    sortBy?: string;
+    order?: 'DESC' | 'ASC';
+    filter?: Array<F>;
+    paging?: {
+        page: number;
+        limit?: number;
+    }
+    slice?: {
+        start: number;
+        end: number;
+        limit?: number;
+    },
+    search?: string;
+}
+
+/**
+ * Создает параметры для запроса к json-server'у
+ */
+export const createRequestParams = function <T>(options: IRequestParams<T>): IQueryParams {
+    const result = {};
+
+    if (!options) return result;
+
+    // Добавление параметра сортировки
+    if (options.sortBy) result["_sort"] = options.sortBy;
+
+    // Добавление параметров фильтрации
+    if (options.filter) appendFilterParams(result, options.filter);
+
+    // Добавление параметров выборки секвенции
+    if (options.slice && options.slice.hasOwnProperty('start') && options.slice.hasOwnProperty('end')) {
+        result["_start"] = options.slice.start;
+        result["_end"] = options.slice.end;
+        if (options.slice.hasOwnProperty('limit')) result["_limit"] = options.slice.limit;
+    }
+
+    // Добавление параметров пагинации
+    if (options.paging && options.paging.hasOwnProperty('page')) {
+        result["_page"] = options.paging.page;
+        if (options.paging.hasOwnProperty('limit')) result["_limit"] = options.paging.limit;
+    }
+
+    if (options.hasOwnProperty('search')) result["q"] = options.search;
+
+    return result;
+}
+
+/**
+ * Добавляет к объекту src параметры фильтрации
+ */
+const appendFilterParams = function <T>(src: any, filter: Array<T>): IQueryParams {
+    if (!src) src = {};
+
+    if (!filter) return;
+
+    filter.forEach((v: any) => {
+        Object.keys(v).forEach(k => {
+            const val = String(v[k]);
+            if (src.hasOwnProperty(k)) src[k].push(val)
+            else src[k] = [val];
+        });
+    });
+
+    return src;
+}
